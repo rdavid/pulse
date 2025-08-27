206f8a2c328f4b974175be7ad02c537e06b70c00
David Rabkin <david@rabkin.co.il>
2025-08-28 02:28:02 +0300

    feat(netcat): add sending and receiving data

--- client/client.sh
+++ client/client.sh
@@ -9,17 +9,30 @@
 # shellcheck disable=SC3043 # Uses local variables.
 # shellcheck disable=SC1091,SC2034 # File not following, variable unused.
 readonly \
-	BASE_APP_VERSION=0.9.20250827
+	BASE_APP_VERSION=0.9.20250828 \
+	INTERVAL=5 \
+	SERVER_HOST="${SERVER_HOST:-server}" \
+	SERVER_PORT="${SERVER_PORT:-8080}"
 . base.sh
 
-# Cycle.
-main() {
-	local i=0
+# Sends data to the server every INTERVAL seconds.
+loop() {
+	local cnt=0 lne msg
 	while :; do
-		log "$(whoami)" "$SERVER_HOST":"$SERVER_PORT" "$i".
-		sleep 5
-		i=$((i + 1))
+		lne="$(prettyuptime)"
+		msg="$(printf %s\\n "$lne" | nc 2>&1 "$SERVER_HOST" "$SERVER_PORT")" ||
+			loge E "$msg"
+		cnt=$((cnt + 1))
+		log "Sent $cnt times, wait for ${INTERVAL}s."
+		sleep "$INTERVAL"
 	done
+}
+
+# Validates and sends data in a loop.
+main() {
+	validate_cmd nc
+	log "$(whoami)" is sending data to "$SERVER_HOST":"$SERVER_PORT".
+	loop
 }
 
 # Starting point.
--- server/server.sh
+++ server/server.sh
@@ -9,17 +9,34 @@
 # shellcheck disable=SC3043 # Uses local variables.
 # shellcheck disable=SC1091,SC2034 # File not following, variable unused.
 readonly \
-	BASE_APP_VERSION=0.9.20250827
+	BASE_APP_VERSION=0.9.20250828 \
+	LISTEN_PORT="${LISTEN_PORT:-8080}"
 . base.sh
 
-# Cycle.
-main() {
-	local i=0
+# Loop
+loop() {
+	local cnt=0 lne
 	while :; do
-		log "$(whoami)" "$LISTEN_HOST":"$LISTEN_PORT" "$i".
-		sleep 5
-		i=$((i + 1))
+		nc -l -p "$LISTEN_PORT" | {
+			IFS= read -r lne
+			[ -n "$lne" ] && store "$lne"
+		}
+		cnt=$((cnt + 1))
+		log Stored "$cnt" times.
+		sleep 1
 	done
+}
+
+# Validates and listens in a loop.
+main() {
+	validate_cmd nc
+	log "$(whoami)" is listening on "$LISTEN_PORT".
+	loop
+}
+
+# Keeps the first argument in DB.
+store() {
+	log DB: "$1"
 }
 
 # Starting point.
